641157b3c1a4066bb0eb77414511d97db91a6b4d
Brittney Kernan <misskernan@gmail.com>
2024-01-26 14:14:57 -0500

    feat(icon): install

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { astroImageTools } from 'astro-imagetools';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,5 +24,6 @@ export default defineConfig({
     sitemap(),
     robotsTxt(),
     astroImageTools,
+    icon()
   ],
 });
