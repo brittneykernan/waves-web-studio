2f2bcb12b0317a6d292b864b8bc88f5db8e4c431
Brittney Kernan <misskernan@gmail.com>
2024-01-27 15:07:07 -0500

    feat(animation): fixing project animation issues

--- src/partials/RightRailMessage.tsx
+++ src/partials/RightRailMessage.tsx
@@ -1,9 +1,12 @@
 type IProps = {
   children: string;
+  className?: string;
 };
 
 const RightRailMessage = (props: IProps) => (
-  <span className="right-rail-message hidden lg:flex">{props.children}</span>
+  <span className={`right-rail-message hidden lg:flex ${props.className}`}>
+    {props.children}
+  </span>
 );
 
 export default RightRailMessage;
--- src/partials/WaveButton.tsx
+++ src/partials/WaveButton.tsx
@@ -1,11 +1,12 @@
 type IWaveButtonProps = {
   children: string;
+  className?: string;
 };
 
 const WaveButton = (props: IWaveButtonProps) => (
   <a
     href="#"
-    className="waveButton fadeUpIn relative shrink-0 self-start overflow-hidden rounded-full border-2 border-black px-4 py-2 text-lg"
+    className={`waveButton relative shrink-0 self-start overflow-hidden rounded-full border-2 border-black px-4 py-2 text-lg ${props.className}`}
   >
     {props.children}
   </a>
