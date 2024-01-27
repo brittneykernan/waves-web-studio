f13fcda35bfec4c6c956678d98ac9695c157bb5c
Brittney Kernan <misskernan@gmail.com>
2024-01-27 17:59:04 -0500

    feat(chore): data, tools, component refactor

---
+++ src/data/tools.js
@@ -0,0 +1,27 @@
+const Tools = [
+  { name: 'GSAP', filename: 'gsap' }, // duplicated because you cannot see them
+  { name: 'Astro.js', filename: 'astrojs' },
+  { name: 'Tailwind CSS', filename: 'tailwindcss' },
+  { name: 'TypeScript', filename: 'typescript' },
+  { name: 'styled-components', filename: 'styled-components' },
+  { name: 'React', filename: 'react' },
+  { name: 'Wordpress', filename: 'wordpress' },
+  { name: 'Figma', filename: 'figma' },
+  { name: 'Squarespace', filename: 'squarespace' },
+  { name: 'Shopify', filename: 'shopify' },
+  { name: 'Webflow', filename: 'webflow' },
+  { name: 'Contentful', filename: 'contentful' },
+  { name: 'SASS', filename: 'sass' },
+  { name: 'Next.js', filename: 'nextjs' },
+  { name: 'GSAP', filename: 'gsap' },
+  { name: 'Astro.js', filename: 'astrojs' }, // duplicated because you cannot see them
+  // {name: "Framer", filename: "framer" },
+  // { name: 'HTML', filename: 'html' },
+  // { name: 'CSS', filename: 'css' },
+  // { name: 'JavaScript', filename: 'javascript' },
+  // { name: 'Firebase', filename: 'firebase' },
+  // { name: 'AWS', filename: 'aws' },
+  // { name: 'Google Analytics', filename: 'google-analytics' },
+];
+
+export default Tools;
